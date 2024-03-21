982f743ebde0e6a4794d2eab547a4196c356655b
Chamberboi <trinmhe151037@fpt.edu.vn>
2024-03-21 09:04:08 +0700

    scroll view

--- app/src/main/java/com/example/easylicence/RandomQuizActivity.java
+++ app/src/main/java/com/example/easylicence/RandomQuizActivity.java
@@ -6,45 +6,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.easylicence.R;
 import com.example.easylicence.models.Quiz;
 import com.example.easylicence.models.QuizData;
-
 import java.util.List;
 import java.util.Locale;
 
 public class RandomQuizActivity extends AppCompatActivity {
-    private static final long TOTAL_TIME_MILLISECONDS = 20 * 60 * 1000; // 20 phút expressed in milliseconds
-    // Khai báo các view
-    TextView questionTextView;
+    private static final long TOTAL_TIME_MILLISECONDS = 20 * 60 * 1000;
+    TextView questionTextView, totalQuestionTextView, timerTextView;
     Button btn1, btn2, btn3, btn4, btnNext;
-    TextView totalQuestionTextView, timerTextView;
-    // Khai báo danh sách câu hỏi
     List<Quiz> quizList;
-    // Biến đếm câu hỏi
     int currentQuestionIndex = 0;
-    // Biến đếm thời gian
-    private static final long COUNTDOWN_IN_MILLIS = 60000; // 60 seconds
+    private static final long COUNTDOWN_IN_MILLIS = 60000;
     private CountDownTimer countDownTimer;
     private long timeLeftInMillis = COUNTDOWN_IN_MILLIS;
     int correctAnswers = 0;
 
     private void bindingView() {
         questionTextView = findViewById(R.id.question_textview);
+        totalQuestionTextView = findViewById(R.id.total_question);
+        timerTextView = findViewById(R.id.timer_indicator_textview);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
         btnNext = findViewById(R.id.btnNext);
-        totalQuestionTextView = findViewById(R.id.total_question);
-        timerTextView = findViewById(R.id.timer_indicator_textview);
-
     }
 
     private void bindingAction() {
-
         btn1.setOnClickListener(this::onBtn1Click);
         btn2.setOnClickListener(this::onBtn2Click);
         btn3.setOnClickListener(this::onBtn3Click);
@@ -52,19 +44,14 @@ public class RandomQuizActivity extends AppCompatActivity {
         btnNext.setOnClickListener(this::onBtnNextClick);
     }
 
-
-
     private void onBtnNextClick(View view) {
         currentQuestionIndex++;
         if (currentQuestionIndex < quizList.size()) {
             displayQuestion();
         } else {
-            // Nếu đã đến câu hỏi cuối cùng, dừng CountdownTimer
             if (countDownTimer != null) {
                 countDownTimer.cancel();
             }
-
-            // Hiển thị kết quả cuối cùng
             int totalQuestions = quizList.size();
             Toast.makeText(RandomQuizActivity.this, "Số câu đúng: " + correctAnswers + "/" + totalQuestions, Toast.LENGTH_SHORT).show();
         }
@@ -93,52 +80,33 @@ public class RandomQuizActivity extends AppCompatActivity {
         }
     }
 
-//    private void displayQuestion() {
-//        Quiz currentQuiz = quizList.get(currentQuestionIndex);
-//        questionTextView.setText(currentQuiz.getQuestion());
-//        btn1.setText(currentQuiz.getOption1());
-//        btn2.setText(currentQuiz.getOption2());
-//        btn3.setText(currentQuiz.getOption3());
-//        btn4.setText(currentQuiz.getOption4());
-//
-//        // Cập nhật bộ đếm câu hỏi
-//        totalQuestionTextView.setText(String.format(Locale.getDefault(), "Question %d/%d", currentQuestionIndex + 1, quizList.size()));
-//    }
-
     private void displayQuestion() {
         Quiz currentQuiz = quizList.get(currentQuestionIndex);
         questionTextView.setText(currentQuiz.getQuestion());
-
-        // Hiển thị đáp án nếu chúng được cung cấp
         if (currentQuiz.getOption1() != null && !currentQuiz.getOption1().isEmpty()) {
             btn1.setVisibility(View.VISIBLE);
             btn1.setText(currentQuiz.getOption1());
         } else {
             btn1.setVisibility(View.GONE);
         }
-
         if (currentQuiz.getOption2() != null && !currentQuiz.getOption2().isEmpty()) {
             btn2.setVisibility(View.VISIBLE);
-            btn2.setText( currentQuiz.getOption2());
+            btn2.setText(currentQuiz.getOption2());
         } else {
             btn2.setVisibility(View.GONE);
         }
-
         if (currentQuiz.getOption3() != null && !currentQuiz.getOption3().isEmpty()) {
             btn3.setVisibility(View.VISIBLE);
             btn3.setText(currentQuiz.getOption3());
         } else {
             btn3.setVisibility(View.GONE);
         }
-
         if (currentQuiz.getOption4() != null && !currentQuiz.getOption4().isEmpty()) {
             btn4.setVisibility(View.VISIBLE);
             btn4.setText(currentQuiz.getOption4());
         } else {
             btn4.setVisibility(View.GONE);
         }
-
-        // Cập nhật bộ đếm câu hỏi
         totalQuestionTextView.setText(String.format(Locale.getDefault(), "Question %d/%d", currentQuestionIndex + 1, quizList.size()));
     }
 
@@ -146,25 +114,19 @@ public class RandomQuizActivity extends AppCompatActivity {
         countDownTimer = new CountDownTimer(TOTAL_TIME_MILLISECONDS, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                // Update the TextView displaying the remaining time
                 updateCountDownText(millisUntilFinished);
             }
 
             @Override
             public void onFinish() {
-                // Handle when the countdown timer finishes
-                // For example, display a message or end the quiz
-                // You can add your logic here
                 Toast.makeText(RandomQuizActivity.this, "Time's up!", Toast.LENGTH_SHORT).show();
             }
         }.start();
     }
+
     private void updateCountDownText(long millisUntilFinished) {
-        // Convert milliseconds to minutes and seconds
         long minutes = millisUntilFinished / 60000;
         long seconds = (millisUntilFinished % 60000) / 1000;
-
-        // Format the time as "mm:ss" and display it in the TextView
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         timerTextView.setText(timeLeftFormatted);
     }
@@ -181,20 +143,10 @@ public class RandomQuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_quiz);
-
-        // Ánh xạ các view từ layout
         bindingView();
         bindingAction();
-        // Khởi tạo danh sách câu hỏi từ dữ liệu được sinh ra
         quizList = QuizData.generateQuizList();
-        // Hiển thị câu hỏi đầu tiên
         displayQuestion();
-        // Khởi tạo bộ đếm thời gian
         startCountdownTimer();
-
-
-
-
     }
-
 }
